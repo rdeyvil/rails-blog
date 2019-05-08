f3b84dd2e4d7423638155ae58821775a11881569
rdeyvil <rdeyvil11@gmail.com>
2019-05-08 11:37:02 +0545

    create user and migration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,55 +1,59 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update] 
+  def index 
+    @article = Article.all
+    render 'index'
+  end 
 
-def index 
-  @article = Article.all
-  render 'index'
-end 
-
-def new
-  @article = Article.new
-end
-
-def create
-  # render plain: params[:article] 
-     
-  #  binding.pry
-     
-  @article = Article.new(article_params)
-
-  #  binding.pry
-  if @article.save   
-    flash[:notice] = "Article Successfully created."
-    redirect_to article_path(@article) 
-  else
-    flash[:alert] = "Failed to create."
-    render 'new'
+  def new
+    @article = Article.new
   end
-end
 
-
-def show
-   @article = Article.find(params[:id])
-end
-
-def edit
-  @article = Article.find(params[:id])
-end
-
-def update
-  @article = Article.find(params[:id])
-    if @article.update(article_params)
-      flash[:notice] = "Article was sucessfully updated"
-      redirect_to article_path(@article)
+  def create
+    # render plain: params[:article] 
+    @article = Article.new(article_params)
+    if @article.save   
+      flash[:notice] = "Article Successfully created."
+      redirect_to article_path(@article) 
     else
-      flash[:alert] = "Failed to update"
-      render 'edit'
+      flash[:alert] = "Failed to create."
+      render 'new'
     end
-end
+  end
 
-private
-  def article_params
-    params.require(:article).permit(:title, :description)
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+      if @article.update(article_params)
+        flash[:notice] = "Article was sucessfully updated"
+        redirect_to article_path(@article)
+      else
+        flash[:alert] = "Failed to update"
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @article.destroy
+    flashp[:notice] = "Article was sucessfully deleted"
+    redirect_to articles_path
+  end
+
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
     end
+    def article_params
+      params.require(:article).permit(:title, :description)
+      end
 
 end
 
