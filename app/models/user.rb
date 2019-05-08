17b96af1de5389037be9ff3b9310894803a0ddb1
rdeyvil <rdeyvil11@gmail.com>
2019-05-08 12:26:06 +0545

    User Validations: Email Regex left

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+             uniqueness: {case_sensitive: false},
+             length: {minimum: 3, maximum:25 }
+  
+  #VALID_EMAIL_REGEX: 
+
+  validates :email, presence: true, length: { maximum: 105},
+            uniqueness: { case_sensetive: false }
+            #format: { with: VALID_EMAIL_REGEX }
 end
