041b03d5371853ee923a1eb7e67c6d910c99e453
rdeyvil <rdeyvil11@gmail.com>
2019-05-26 22:37:36 +0545

    User created with all validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
              uniqueness: {case_sensitive: false},
              length: {minimum: 3, maximum:25 }
   
-  #VALID_EMAIL_REGEX: 
-
+  VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105},
-            uniqueness: { case_sensetive: false }
-            #format: { with: VALID_EMAIL_REGEX }
+            uniqueness: { case_sensetive: false },
+            format: { with: VALID_EMAIL_REGEX }
 end
