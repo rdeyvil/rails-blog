f85f65a063acd73e76634ad682275a943370ad83
rdeyvil <rdeyvil11@gmail.com>
2019-06-05 16:41:35 +0545

    Edit/Updates Users from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
 
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account is sucessfully updated"
+      redirect_to articles_path
+    else
+    
+    end 
+  end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
